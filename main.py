b05d7ed41e21854176c24b44c141c2f0382b6487
lan21 <allan.rakotoarivony@gmail.com>
2023-05-26 20:36:51 +0200

    day 9: use if instead of match case

--- main.py
+++ main.py
@@ -11,48 +11,45 @@
 
 
 while True:
-    action = input("Type add, show, edit, complete or exit: ")
-
+    user_input = input("Type add, show, edit, complete or exit: ").strip()
     todos_filename = "todos.txt"
-    match action.strip():
-        case "add":
-            todo = input("Enter a todo: ") + "\n"
-            with open(todos_filename, "r") as file:
-                todos = file.readlines()
-            todos.append(todo)
-            with open(todos_filename, "w") as file:
-                file.writelines(todos)
-        case "show" | "display":
-            with open(todos_filename, "r") as file:
-                todos = file.readlines()
-            todos = [item.strip("\n") for item in todos]
-            for index, todo in enumerate(todos):
-                row = f"{index + 1}-{todo.capitalize()}"
-                print(row)
-        case "edit":
-            number = int(input("Enter the number you want to edit: "))
-            number = number - 1
-            new_todo = input("Enter the new value: ")
 
-            with open(todos_filename, "r") as file:
-                todos = file.readlines()
+    if "add" in user_input:
+        todo = user_input[4:] + "\n"
+        with open(todos_filename, "r") as file:
+            todos = file.readlines()
+        todos.append(todo)
+        with open(todos_filename, "w") as file:
+            file.writelines(todos)
+    if "show" in user_input:
+        with open(todos_filename, "r") as file:
+            todos = file.readlines()
+        todos = [item.strip("\n") for item in todos]
+        for index, todo in enumerate(todos):
+            row = f"{index + 1}-{todo.capitalize()}"
+            print(row)
+    if "edit" in user_input:
+        number = int(input("Enter the number you want to edit: "))
+        number = number - 1
+        new_todo = input("Enter the new value: ")
 
-            todos[number] = new_todo + "\n"
+        with open(todos_filename, "r") as file:
+            todos = file.readlines()
 
-            with open(todos_filename, "w") as file:
-                file.writelines(todos)
+        todos[number] = new_todo + "\n"
 
-        case "complete":
-            number = int(input("Enter the number you want to complete: "))
-            with open(todos_filename, "r") as file:
-                todos = file.readlines()
-            removed_todo = todos.pop(number - 1).strip("\n")
-            with open(todos_filename, "w") as file:
-                file.writelines(todos)
-            message = f"Todo {removed_todo} was marked as completed"
-            print(message)
-        case "exit":
-            break
-        case other_command:
-            print("Command: " + other_command + " not recognized")
+        with open(todos_filename, "w") as file:
+            file.writelines(todos)
+
+    if "complete" in user_input:
+        number = int(input("Enter the number you want to complete: "))
+        with open(todos_filename, "r") as file:
+            todos = file.readlines()
+        removed_todo = todos.pop(number - 1).strip("\n")
+        with open(todos_filename, "w") as file:
+            file.writelines(todos)
+        message = f"Todo {removed_todo} was marked as completed"
+        print(message)
+    if "exit" in user_input:
+        break
 print("Bye!")
