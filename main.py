5d555522bd0c9e390b42b1f941a0e1086dc4b1fc
lan21 <allan.rakotoarivony@gmail.com>
2023-06-12 17:32:42 +0200

    day 10: add try except

--- main.py
+++ main.py
@@ -21,37 +21,47 @@ while True:
         todos.append(todo)
         with open(todos_filename, "w") as file:
             file.writelines(todos)
-    elif user_input.startswith("show "):
+
+    elif user_input.startswith("show"):
         with open(todos_filename, "r") as file:
             todos = file.readlines()
         todos = [item.strip("\n") for item in todos]
         for index, todo in enumerate(todos):
             row = f"{index + 1}-{todo.capitalize()}"
             print(row)
+
     elif user_input.startswith("edit "):
-        number = int(user_input[5:])
-        number = number - 1
-        new_todo = input("Enter the new value: ")
+        try:
+            number = int(user_input[5:])
+            number = number - 1
+            new_todo = input("Enter the new value: ")
 
-        with open(todos_filename, "r") as file:
-            todos = file.readlines()
+            with open(todos_filename, "r") as file:
+                todos = file.readlines()
 
-        todos[number] = new_todo + "\n"
+            todos[number] = new_todo + "\n"
 
-        with open(todos_filename, "w") as file:
-            file.writelines(todos)
+            with open(todos_filename, "w") as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Invalid command")
 
     elif user_input.startswith("complete "):
-        number = int(user_input[9:])
-        with open(todos_filename, "r") as file:
-            todos = file.readlines()
-        removed_todo = todos.pop(number - 1).strip("\n")
-        with open(todos_filename, "w") as file:
-            file.writelines(todos)
-        message = f"Todo {removed_todo} was marked as completed"
-        print(message)
+        try:
+            number = int(user_input[9:])
+            with open(todos_filename, "r") as file:
+                todos = file.readlines()
+            removed_todo = todos.pop(number - 1).strip("\n")
+            with open(todos_filename, "w") as file:
+                file.writelines(todos)
+            message = f"Todo {removed_todo} was marked as completed"
+            print(message)
+        except IndexError:
+            print("There is no value with that index")
+
     elif "exit" in user_input:
         break
+        
     else:
         print(f"Unknown command: {user_input}")
 print("Bye!")
