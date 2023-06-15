5f5852d93d5462d067e01cc1770eb208d26c6d5b
lanbuhti <allan.rakotoarivony@gmail.com>
2023-06-15 22:22:16 +0200

    day 13: add doc strings

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def get_todos(filename="todos.txt"):
+    """ Read a text file and return the list of to-do items. """
     with open(filename, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, filename="todos.txt"):
+    """ Write the to-do items list in a text file. """
     with open(filename, "w") as file:
         file.writelines(todos_arg)
 
