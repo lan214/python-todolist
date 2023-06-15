d1be62a6e88417282c3fd0c9df2a0fab78a83576
Allan Rakotoarivony <lan214@Allans-MacBook-Pro.local>
2023-06-15 17:19:42 +0200

    day 12: use function parameters

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-def get_todos():
-    with open("todos.txt", "r") as file_local:
+def get_todos(filename):
+    with open(filename, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
+def write_todos(filename, todos_arg):
+    with open(filename, "w") as file:
+        file.writelines(todos_arg)
+
+
 while True:
     user_input = input("Type add, show, edit, complete or exit: ").strip()
-    todos_filename = "todos.txt"
 
     if user_input.startswith("add "):
         todo = user_input[4:] + "\n"
-        todos = get_todos()
+        todos = get_todos("todos.txt")
         todos.append(todo)
-        with open(todos_filename, "w") as file:
-            file.writelines(todos)
+        write_todos("todos.txt", todos)
 
     elif user_input.startswith("show"):
-        todos = get_todos()
+        todos = get_todos("todos.txt")
         todos = [item.strip("\n") for item in todos]
         for index, todo in enumerate(todos):
             row = f"{index + 1}-{todo.capitalize()}"
@@ -28,21 +31,18 @@ while True:
             number = number - 1
             new_todo = input("Enter the new value: ")
 
-            todos = get_todos()
+            todos = get_todos("todos.txt")
             todos[number] = new_todo + "\n"
-
-            with open(todos_filename, "w") as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos)
         except ValueError:
             print("Invalid command")
 
     elif user_input.startswith("complete "):
         try:
             number = int(user_input[9:])
-            todos = get_todos()
+            todos = get_todos("todos.txt")
             removed_todo = todos.pop(number - 1).strip("\n")
-            with open(todos_filename, "w") as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos)
             message = f"Todo {removed_todo} was marked as completed"
             print(message)
         except IndexError:
@@ -50,7 +50,7 @@ while True:
 
     elif "exit" in user_input:
         break
-        
+
     else:
         print(f"Unknown command: {user_input}")
 print("Bye!")
