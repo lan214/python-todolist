cc439f0363555cce82568cd26dcb3e0b713ea5f3
lan214 <allan.rakotoarivony@gmail.com>
2023-06-16 21:40:35 +0200

    day 14: add if __name__ to imported module

--- functions.py
+++ functions.py
@@ -9,3 +9,8 @@ def write_todos(todos_arg, filename="todos.txt"):
     """ Write the to-do items list in a text file. """
     with open(filename, "w") as file:
         file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
