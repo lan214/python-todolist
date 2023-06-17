c588465570a5e6eb4e6d33bb5fc7b070acb75897
lan214 <allan.rakotoarivony@gmail.com>
2023-06-17 23:05:17 +0200

    Day 15: add exercise using random module

---
+++ exercises/exo15.py
@@ -0,0 +1,7 @@
+import random
+
+lower_bound = int(input("Enter the lower bound: "))
+upper_bound = int(input("Enter the upper bound: "))
+
+number = random.randint(lower_bound, upper_bound)
+print(number)
