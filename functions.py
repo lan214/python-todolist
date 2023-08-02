c028f5bb0fd3b596b9f9f2c298e5af246f850ecf
lan214 <allan.rakotoarivony@gmail.com>
2023-08-02 17:28:00 +0200

    day 17: implement add button

--- functions.py
+++ functions.py
@@ -1,11 +1,14 @@
-def get_todos(filename="todos.txt"):
+FILENAME = "todos.txt"
+
+
+def get_todos(filename=FILENAME):
     """ Read a text file and return the list of to-do items. """
     with open(filename, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filename="todos.txt"):
+def write_todos(todos_arg, filename=FILENAME):
     """ Write the to-do items list in a text file. """
     with open(filename, "w") as file:
         file.writelines(todos_arg)
--- gui.py
+++ gui.py
@@ -2,9 +2,21 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Enter a todo")
-input_box = sg.Input(tooltip="Enter todo")
+input_box = sg.Input(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My app", layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window("My app",
+                   layout=[[label], [input_box, add_button]],
+                   font=("Helvetica", 20))
+while True:
+    event, value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            todos.append(value['todo'] + "\n")
+            functions.write_todos(todos)
+        case sg.WINDOW_CLOSED:
+            break
 window.close()
