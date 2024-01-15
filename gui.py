aac6dce039052e0dd262657f6d8d28629375a458
lan214 <allan.rakotoarivony@gmail.com>
2024-01-15 19:57:03 +0100

    day 18: implement complete and exit

--- gui.py
+++ gui.py
@@ -6,9 +6,14 @@ input_box = sg.Input(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", size=(45, 10), enable_events=True)
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My app",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 20))
 while True:
     event, value = window.read()
@@ -28,9 +33,18 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = value['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
         case "todos":
             selected_todo = value['todos'][0][:-1]
             window['todo'].update(selected_todo)
+        case "Exit":
+            break
         case sg.WINDOW_CLOSED:
             break
 window.close()
